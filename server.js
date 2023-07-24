9d04d73e8bf7a4551fbc399b1d6834843a059a2a
balderekjk <bal.derek.j@gmail.com>
2023-07-24 12:16:17 -1000

    fix bug

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const WebSocket = require("ws");
-var os = require("os");
+// var os = require("os");
 var pty = require("node-pty");
 
 const wss = new WebSocket.Server({ port: 3050 });
 
 console.log("Socket is up and running...");
 
-var shell = os.platform() === "bash";
+var shell = "bash";
 var ptyProcess = pty.spawn(shell, [], {
   name: "xterm-color",
   cwd: process.env.HOME,
