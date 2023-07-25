cde0ad8fab0210de745afb253e2db147df26d890
balderekjk <bal.derek.j@gmail.com>
2023-07-25 11:22:23 -1000

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var ptyProcess = pty.spawn(shell, [], {
   echo: false,
 });
 wss.on("connection", (ws) => {
-  ws.send(data);
+  ws.send(`Successfully connected to ${shell} (Press "Enter")`);
   ws.on("message", (command) => {
     ptyProcess.write(command);
   });
