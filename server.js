4fd81bdd9a04ec41a5b84db7c10314fbf10d64b9
balderekjk <bal.derek.j@gmail.com>
2023-07-24 14:39:36 -1000

    test

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ console.log("Socket is up and running...");
 var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 var ptyProcess = pty.spawn(shell, [], {
   name: "xterm-color",
-  //   cwd: process.env.HOME,
+  cwd: process.env.HOME,
   env: process.env,
-  echo: false,
 });
 wss.on("connection", (ws) => {
   console.log("new session");
-  //   ws.on("message", (command) => {
-  //     ptyProcess.write(command);
-  //   });
+  ws.on("message", (command) => {
+    ptyProcess.write(command);
+  });
 
   ptyProcess.on("data", function (data) {
     ws.send(data);
