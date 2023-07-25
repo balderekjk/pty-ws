1f7274f23038a415772e5c76aed4076c9d3c4870
balderekjk <bal.derek.j@gmail.com>
2023-07-25 11:12:32 -1000

    test

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var pty = require("node-pty");
 
 const wss = new WebSocket.Server({ port: 8000 });
 
-console.log("Socket is up and running...");
-
 var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 var ptyProcess = pty.spawn(shell, [], {
   name: "xterm-color",
@@ -14,16 +12,13 @@ var ptyProcess = pty.spawn(shell, [], {
   echo: false,
 });
 wss.on("connection", (ws) => {
-  console.log("new session");
+  ptyProcess.write(`Successfully connected to ${shell}`);
   ws.on("message", (command) => {
     ptyProcess.write(command);
   });
-});
-ptyProcess.on("data", function (data) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
+
+  ptyProcess.on("data", function (data) {
+    ws.send(data);
+    console.log(data);
   });
-  console.log(data);
 });
