8760c44d2e62882ccda167948d97c07c7bc40d96
balderekjk <bal.derek.j@gmail.com>
2023-07-25 13:27:16 -1000

    3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const WebSocket = require("ws");
 var os = require("os");
 var pty = require("node-pty");
 
-const wss = new WebSocket.Server({ port: 8000 });
+const wss = new WebSocket.Server({ port: 3000 });
 
 var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 var ptyProcess = pty.spawn(shell, [], {
